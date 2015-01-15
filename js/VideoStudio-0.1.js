c2eb8dd72b29f4c876fde141fda18211af113d23
Richard Milewski <richard@mozilla.com>
2015-01-14 17:50:23 -0800

    Toggle Selection
    Dropped use of BLANK buttons.  Added clips to stand in for live cameras.

--- js/VideoStudio-0.1.js
+++ js/VideoStudio-0.1.js
@@ -8,31 +8,44 @@
  
 $( document ).ready(function() {
 	
-	var microphone = 0;
-	var camera     = 0;
-	var layer0source = "";
-	var layer1source = "";
-	var layer2source = "";
-	var layer3source = "";
-	var layer4source = "";
-	var layer5source = "";
-
-	var canvas = $("#pgmCanvas");
-	var ctx = canvas.getContext("2d");
-	var image = document.getElementById('source');
-	/*
-	ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+	var microphone = camera = slide = overlay = 0;
+	var lastCamera = 9;
 	
-	function redraw(){
-		ctx.drawImage(layer0source, 0, 0, 100%, 100%, 0, 0, 100%, 100%);
-		ctx.drawImage(layer4source, 0, 0, 100%, 100%, 0, 0, 100%, 100%);
+	var canvas = document.getElementById('pgmCanvas');
+	var ctx = canvas.getContext("2d");
+	var layer1 = document.getElementById('layer1');
+	var layer2 = document.getElementById('layer2');
+	var layer4 = document.getElementById('layer4');
+	
+	var nullImg = "./slides/blank.png";   
+	
+	var frame = 0 ;
+	
+	function drawFrame(){
+			ctx.clearRect(0,0,1280,720);
+			ctx.drawImage(layer1, 0, 0, 1280, 720, 0, 0, 1280, 720);
+			
+			if (layer2.readyState === layer2.HAVE_ENOUGH_DATA) {
+				ctx.drawImage(layer2, 0, 0, layer2.videoWidth, layer2.videoHeight, 0, 0, 1280, 720);
+			}
+			
+			ctx.drawImage(layer4, 0, 0, 1280, 720, 0, 0, 1280, 720);
+			frameLoop();
+	}
+
+	function frameLoop () {
+		frame++;
+		requestAnimationFrame(drawFrame);
 	}
 	
-	*/
+	frameLoop();
 	
-	//Initialize in audo-follow mode
-	$("#mic0").addClass("activeMicrophone");
 	
+	//Initialize 
+	$("#mic0").addClass("activeMicrophone");  //auto-follow mode
+	$("#slide0").addClass("activeLayer");
+	$("#overlay0").addClass("activeLayer");
+	$("#clip0").addClass("activeLayer");
 
 
 	$( ".microphone" ).click(function( event ) {
@@ -42,48 +55,65 @@ $( document ).ready(function() {
 	$( "#mic0, #mic1, #mic2").click(function (event) {
 		$(this).addClass("activeMicrophone");
 		//microphone = (this.attr("id")substring-after(this.attr("id"), "#mic");
-		microphone = parseInt($(this).attr("id").substr(3));
+		microphone = parseInt($(this).attr("id").substr(3), 10);
 		
 		// Code here to select audio source
 		// if microphone == 0, then use audio from current camera
-		
 	});
 	
-	$( ".camera" ).click(function( event ) {
-		$(".camera").removeClass("activeCamera");
+	$( ".cameraShot" ).click(function( event ) {
+		$(".cameraShot").removeClass("activeCamera");
 	});
 	
-	$( "#cam1, #cam2, #cam3").click(function (event) {
-		$(this).addClass("activeCamera");
-		camera = parseInt($(this).attr("id").substr(3));
-		console.log("camera: "+camera);
+	
+	$( "#cam1, #cam2, #cam3").click(function( event ) {
+		var element = $(this);
+		camera = parseInt(element.attr("id").substr(3), 10);  //find the camera number
 		
+		if (camera === lastCamera) {
+			element.removeClass("activeCamera");
+			$("#layer2").attr("src", "");				// remove source from video layer
+		} else {
+			element.addClass("activeCamera");
+			$("#layer2").attr("src", element.attr("src"));	//set video layer source to this camera
+			lastCamera = camera;
+		}
+			
 		// Code here to select video source
 		// if microphone == 0, then use audio from current camera
+	
+	});
+	
 		
-	});
 	
-         
-	
-    $(".overlay, .slide").on("click", function(e){  //background and overlay handler
-	    
-	    var element = $(this);
-	    var elementClass = element.attr("class");
+    $(".slide").on("click", function(e){  //background handler
+		var element = $(this);
 		var thisImage = element.attr("src");
-        
-        $("."+elementClass).removeClass("activeLayer");	//deselect all images of this class
-        
-        if (elementClass == "slide") {
-            layer0source = thisImage;         			//set background layer source to this image
-        } else {
-            layer4source = thisImage;					//set overlay layer source to this image
+		if (element.hasClass("activeLayer")) {		//Is alreay active so de-activate all.
+			$("#layer1").attr("src", nullImg);
+			element.removeClass("activeLayer");
+		} else {
+        	$(".slide").removeClass("activeLayer");	//clear the green borders
+			element.addClass("activeLayer");		//make this slide active
+			$("#layer1").attr("src", thisImage);	//set background layer source to this image
         }
-        element.addClass("activeLayer"); 			 	//highlight this element
-        
-        var thisImage = element.attr("src");
-        $('#layer1').attr("src", thisImage);    	 //set PGM overlay layer to this overlay
-           	
 	});
+	
+
+	
+	$(".overlay").on("click", function(e){  //overlay handler
+		var element = $(this);
+		var thisImage = element.attr("src");
+		if (element.hasClass("activeLayer")) {		//Is alreay active so de-activate all.
+			$("#layer4").attr("src", nullImg);
+			element.removeClass("activeLayer");
+		} else {
+        	$(".overlay").removeClass("activeLayer");	//clear the green borders
+			element.addClass("activeLayer");			//make this overlay active
+			$("#layer4").attr("src", thisImage);		//set overlay layer source to this image
+        }
+	});
+	
 	
 
 	
