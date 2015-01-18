d505e6cbe508ca4ecaf667320c6d13ce120ded3f
Richard Milewski <richard@mozilla.com>
2015-01-17 23:27:15 -0800

    Image Scaling Mods

--- js/VideoStudio-0.1.js
+++ js/VideoStudio-0.1.js
@@ -8,28 +8,65 @@
  
 $( document ).ready(function() {
 	
+	var format = new Object;
+	format.name = "360p";
+	
 	var microphone = camera = slide = overlay = 0;
-	var lastCamera = 9;
+	var lastCamera = -1;
 	
 	var canvas = document.getElementById('pgmCanvas');
 	var ctx = canvas.getContext("2d");
 	var layer1 = document.getElementById('layer1');
+	layer1.valid = false;
 	var layer2 = document.getElementById('layer2');
 	var layer4 = document.getElementById('layer4');
+	layer4.valid = false;
 	
-	var nullImg = "./slides/blank.png";   
+	switch (format.name) {
+		case "360p":
+			format.width = 640;
+			format.height = 360;
+		break;
+		
+		case "720p":
+			format.width = 1280;
+			format.height = 720;
+		break;
+		
+		case "1080p":
+			format.width = 1920;
+			format.height = 1080;
+	}  
+	
+	$(".layer").attr("height", format.height);
+	$(".layer").attr("width", format.width);
 	
 	var frame = 0 ;
 	
+	layer1.onload = function(){
+			layer1.valid = true;
+	};
+	
+	layer4.onload = function(){
+			layer4.valid = true;
+	};
+	
 	function drawFrame(){
-			ctx.clearRect(0,0,1280,720);
-			ctx.drawImage(layer1, 0, 0, 1280, 720, 0, 0, 1280, 720);
+		
+			ctx.clearRect(0, 0, format.width, format.height);
 			
-			if (layer2.readyState === layer2.HAVE_ENOUGH_DATA) {
-				ctx.drawImage(layer2, 0, 0, layer2.videoWidth, layer2.videoHeight, 0, 0, 1280, 720);
+			if (layer1.valid) {
+				ctx.drawImage(layer1, 0, 0, layer1.width, layer1.height, 0, 0, format.width, format.height);
 			}
 			
-			ctx.drawImage(layer4, 0, 0, 1280, 720, 0, 0, 1280, 720);
+			if (layer2.readyState === layer2.HAVE_ENOUGH_DATA) {
+				ctx.drawImage(layer2, 0, 0, layer2.videoWidth, layer2.videoHeight, 0, 0, format.width, format.height);
+			}
+			
+			if (layer4.valid) {
+				ctx.drawImage(layer4, 0, 0, layer4.width, layer4.height, 0, 0, format.width, format.height);
+			}
+			
 			frameLoop();
 	}
 
@@ -73,6 +110,7 @@ $( document ).ready(function() {
 		if (camera === lastCamera) {
 			element.removeClass("activeCamera");
 			$("#layer2").attr("src", "");				// remove source from video layer
+			lastCamera = -1;
 		} else {
 			element.addClass("activeCamera");
 			$("#layer2").attr("src", element.attr("src"));	//set video layer source to this camera
@@ -86,31 +124,31 @@ $( document ).ready(function() {
 	
 		
 	
-    $(".slide").on("click", function(e){  //background handler
+		$(".slide").on("click", function(e){		//background handler
+        layer1.valid = false;						//mark layer as empty until the next load completes.
 		var element = $(this);
 		var thisImage = element.attr("src");
-		if (element.hasClass("activeLayer")) {		//Is alreay active so de-activate all.
-			$("#layer1").attr("src", nullImg);
+		if (element.hasClass("activeLayer")) {		//Is already active so de-activate all.
 			element.removeClass("activeLayer");
 		} else {
-        	$(".slide").removeClass("activeLayer");	//clear the green borders
+			$(".slide").removeClass("activeLayer");	//clear the green borders
 			element.addClass("activeLayer");		//make this slide active
-			$("#layer1").attr("src", thisImage);	//set background layer source to this image
+			layer1.src = thisImage;					//set background layer source to this image
         }
 	});
 	
 
 	
-	$(".overlay").on("click", function(e){  //overlay handler
+	$(".overlay").on("click", function(e){				//overlay handler
+		layer4.valid = false;							//mark layer as empty until the next load completes.
 		var element = $(this);
 		var thisImage = element.attr("src");
-		if (element.hasClass("activeLayer")) {		//Is alreay active so de-activate all.
-			$("#layer4").attr("src", nullImg);
+		if (element.hasClass("activeLayer")) {			//Is already active so de-activate all.
 			element.removeClass("activeLayer");
 		} else {
-        	$(".overlay").removeClass("activeLayer");	//clear the green borders
+			$(".overlay").removeClass("activeLayer");	//clear the green borders
 			element.addClass("activeLayer");			//make this overlay active
-			$("#layer4").attr("src", thisImage);		//set overlay layer source to this image
+			layer4.src = thisImage;						//set overlay layer source to this image
         }
 	});
 	
