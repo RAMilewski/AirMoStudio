095cd70e030423c2abacfa14e375de9b304faf20
Richard Milewski <richard@mozilla.com>
2015-02-10 13:21:37 -0800

    Minor JS update

--- js/AirMoStudio-0.1.js
+++ js/AirMoStudio-0.1.js
@@ -1,4 +1,4 @@
-// VideoStudio-0.1.js
+// AirMoStudio-0.1.js
 // Copyright 2015 by Richard A. Milewski
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
